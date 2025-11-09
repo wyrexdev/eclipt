ca9b4f94df8ea01058a00e5344c7e3856bf441a2
Wyrex <uchihasasukedev@gmail.com>
2025-11-09 08:30:47 +0000

    Add #pragma once to header files and ensure consistent includes in Entity, Quaternion, Vec2, and Vec3 classes

--- include/Entity/Entity.hpp
+++ include/Entity/Entity.hpp
@@ -1,4 +1,4 @@
-
+#include "Headers/Headers.hpp"
 
 class Entity {
 public:
@@ -8,5 +8,7 @@ public:
     void draw();
 
 private:
-
+    QTX::Vec3 position;
+    QTX::Vec3 rotation;
+    QTX::Vec3 scale;
 };
--- include/Headers/Headers.hpp
+++ include/Headers/Headers.hpp
@@ -10,3 +10,7 @@
 #include "Utils/Glad/Glad.h"
 #include "Entity/Entity.hpp"
 #include "Utils/Enums/Enums.hpp"
+
+#include "QTX/Quaternion/Quaternion.hpp"
+#include "QTX/Vectors/Vec2.hpp"
+#include "QTX/Vectors/Vec3.hpp"
